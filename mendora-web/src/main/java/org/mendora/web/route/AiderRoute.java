b4db38c2f26c302811b39c71a7d57a0a86269165
kam <13710414456@163.com>
2018-03-16 17:44:15 +0800

    引入guice

--- mendora-web/src/main/java/org/mendora/web/route/AiderRoute.java
+++ mendora-web/src/main/java/org/mendora/web/route/AiderRoute.java
@@ -1,5 +1,6 @@
 package org.mendora.web.route;
 
+import com.google.inject.Inject;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.rxjava.core.eventbus.Message;
@@ -20,7 +21,8 @@ import org.mendora.util.constant.WebResult;
 public class AiderRoute implements Route {
     private static final String MODULE = "/mendora/aider";
     private static final String SQL_STATIEMENT = MODULE + "/sqlStatement";
-    private DataAccessService dataAccessService = DataAccessService.createProxy(VertxHolder.vertx());
+    @Inject
+    private DataAccessService dataAccessService;
 
     @Override
     public void route(Router router) {
@@ -28,11 +30,13 @@ public class AiderRoute implements Route {
         router.get(MODULE + "/demo").handler(rc -> {
             rc.response().end("<h1>Just a test demo.</h1>");
         });
+
         // rpc service demo
         router.post(SQL_STATIEMENT + "/query").handler(rc -> {
             dataAccessService.rxQuery(rc.getBodyAsJson().getString(SqlReferences.STATEMENT.val()))
                     .subscribe(replyJson -> WebResult.consume(replyJson, rc));
         });
+
         /** eventbus demo **/
         router.post(SQL_STATIEMENT + "/queryWithParams").handler(rc -> {
             JsonObject doc = rc.getBodyAsJson();
@@ -40,42 +44,49 @@ public class AiderRoute implements Route {
                     .map(Message::body)
                     .subscribe(replyJson -> WebResult.consume(replyJson, rc));
         });
+
         router.post(SQL_STATIEMENT + "/querySingle").handler(rc -> {
             String sqlStatement = rc.getBodyAsJson().getString(SqlReferences.STATEMENT.val());
             VertxHolder.eventBus().<JsonObject>rxSend(EBAddress.DATA_EB_QUERY_SINGLE, sqlStatement)
                     .map(Message::body)
                     .subscribe(replyJson -> WebResult.consume(replyJson, rc));
         });
+
         router.post(SQL_STATIEMENT + "/querySingleWithParams").handler(rc -> {
             JsonObject doc = rc.getBodyAsJson();
             VertxHolder.eventBus().<JsonObject>rxSend(EBAddress.DATA_EB_QUERY_SINGLE_WITH_PARAMS, doc)
                     .map(Message::body)
                     .subscribe(replyJson -> WebResult.consume(replyJson, rc));
         });
+
         router.post(SQL_STATIEMENT + "/update").handler(rc -> {
             String sqlStatement = rc.getBodyAsJson().getString(SqlReferences.STATEMENT.val());
             VertxHolder.eventBus().<JsonObject>rxSend(EBAddress.DATA_EB_UPDATE, sqlStatement)
                     .map(Message::body)
                     .subscribe(replyJson -> WebResult.consume(replyJson, rc));
         });
+
         router.post(SQL_STATIEMENT + "/updateWithParams").handler(rc -> {
             JsonObject doc = rc.getBodyAsJson();
             VertxHolder.eventBus().<JsonObject>rxSend(EBAddress.DATA_EB_UPDATE_WITH_PARAMS, doc)
                     .map(Message::body)
                     .subscribe(replyJson -> WebResult.consume(replyJson, rc));
         });
+
         router.post(SQL_STATIEMENT + "/batchWithParams").handler(rc -> {
             JsonObject doc = rc.getBodyAsJson();
             VertxHolder.eventBus().<JsonObject>rxSend(EBAddress.DATA_EB_BATCH_WITH_PARAMS, doc)
                     .map(Message::body)
                     .subscribe(replyJson -> WebResult.consume(replyJson, rc));
         });
+
         router.post(SQL_STATIEMENT + "/execute").handler(rc -> {
             String sqlStatement = rc.getBodyAsJson().getString(SqlReferences.STATEMENT.val());
             VertxHolder.eventBus().<JsonObject>rxSend(EBAddress.DATA_EB_EXECUTE, sqlStatement)
                     .map(Message::body)
                     .subscribe(replyJson -> WebResult.consume(replyJson, rc));
         });
+
         // get configuration properties list.
         router.get(MODULE + "/config").handler(rc -> {
             WebResult.succ(ConfigHolder.asJson(), rc);
